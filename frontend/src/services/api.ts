c0221f7b98864157fb73280abf279880e04e5277
jiwoni1210 <jiwoni1210@naver.com>
2024-01-18 14:49:13 +0900

    Feat(S10P12A601-84):refresh토큰 재발급 추가

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import useUserStore from '../stores/userStore';
+import refreshAccessToken from './user/refreshAccessToken';
 
 // axios 인스턴스 생성
 const api = axios.create({
@@ -17,5 +18,19 @@ api.interceptors.request.use((config) => {
   return Promise.reject(error);
 });
 
+// 응답 인터셉터 추가
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const { accessToken } = await refreshAccessToken();
+      useUserStore.getState().setAccessToken(accessToken);
+      return api(originalRequest);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
