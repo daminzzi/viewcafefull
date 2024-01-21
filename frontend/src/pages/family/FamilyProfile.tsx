10c004799ec52cb85cc9a2f437be29fdc7df2d6c
jiwon <jiwoni1210@naver.com>
2024-01-21 17:22:41 +0900

    Feat(S10P12A601-88): 보호자 프로필페이지 & 현재 유저 정보 조회 함수 추가

--- frontend/src/pages/family/FamilyProfile.tsx
+++ frontend/src/pages/family/FamilyProfile.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import getUserInfo from '../../services/user/getUserInfo';
+import useConnectStore from "../../stores/ConnectStore";
 
 type Props = {}
 
+interface UserInfo {
+  parentId: number[] | null;
+  parentName: string[] | null;
+  phoneNumber: string;
+  birth: string;
+  userRole: string;
+}
+
+
 function FamilyProfile({}: Props) {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+  const { currConnect } = useConnectStore();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getUserInfo();
+      setUserInfo(res);
+    };
+    fetchData();
+  }, []);
+
+  if (!userInfo) {
+    return <div>Now Loading...</div>;
+  }
+
   return (
-    <div>FamilyProfile</div>
+    <>
+      <div>FamilyProfile</div>
+      <div>이름</div>
+      <div>{userInfo.parentName}</div>
+      <div>생년월일</div>
+      <div>{userInfo.birth}</div>
+      <div>휴대전화</div>
+      <div>{userInfo.phoneNumber}</div>
+      <br />
+      <div>연결된 입소자 목록</div>
+          <p>{currConnect.perName}요양원</p>
+          <p>입소자 이름: {currConnect.tarName} 님</p>
+    </>
   )
 }
 
-export default FamilyProfile
+export default FamilyProfile;
---
+++ frontend/src/services/user/getUserInfo.ts
@@ -0,0 +1,29 @@
+import api from '../api';
+
+interface UserInfo {
+  parentId: number[] | null;
+  parentName: string[] | null;
+  residentId: number | null;
+  residentName: string | null;
+  hospitalId: number;
+  hospitalName: string;
+  phoneNumber: string;
+  birth: string;
+  userRole: string;
+}
+
+// 로그인 된 회원(보호자)의 정보 조회
+async function getUserInfo(): Promise<UserInfo | null> {
+  try {
+    const response = await api.get<UserInfo>(`/users`);
+    if (response.status !== 200) {
+      throw new Error(`오류: ${response.status}`);
+    }
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export default getUserInfo;
