a3e4f7028e194d85d0fcb306b2e8e76aae17d18e
류지원 <jiwoni1210@naver.com>
2024-01-30 12:20:55 +0900

    Design(S10P12A601-178): 페이지네이션 CSS

--- frontend/src/components/common/Pagination.tsx
+++ frontend/src/components/common/Pagination.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as ChevronRight } from '../../assets/icons/chevron-right.svg';
+import { ReactComponent as ChevronLeft } from '../../assets/icons/chevron-left.svg';
+import { white, success, black } from '../../assets/styles/palettes';
 
 type PaginationProps = {
   startPage: number;
-  setStartPage: (pageGroupIndex: number) => void;
+  setStartPage: (setStartPage: number) => void;
   currentPage: number;
   setCurrentPage: (currentPage: number) => void;
   totalPage: number;
 };
 
 // 공통 페이지네이션
-function Pagination({startPage, setStartPage, currentPage, setCurrentPage, totalPage}: PaginationProps) {
+function Pagination({
+  startPage,
+  setStartPage,
+  currentPage,
+  setCurrentPage,
+  totalPage,
+}: PaginationProps) {
   const pageGroupSize = 5;
 
   function showPages() {
@@ -20,28 +30,28 @@ function Pagination({startPage, setStartPage, currentPage, setCurrentPage, total
       i++
     ) {
       pages.push(
-        <button
+        <PageButton
           key={i}
           onClick={() => setCurrentPage(i)}
-          style={{ fontWeight: currentPage === i ? 'bold' : 'normal' }}
+          active={currentPage === i}
         >
           {i}
-        </button>,
+        </PageButton>,
       );
     }
     return pages;
   }
 
   return (
-    <div>
-      <button
+    <Container>
+      <ChevronButton
         onClick={() => setStartPage(Math.max(startPage - 1, 0))}
         disabled={startPage === 0}
       >
-        {'<'}
-      </button>
-      {showPages()}
-      <button
+        <ChevronLeft width="35px" />
+      </ChevronButton>
+      <PagesContainer>{showPages()}</PagesContainer>
+      <ChevronButton
         onClick={() =>
           setStartPage(
             Math.min(
@@ -50,14 +60,44 @@ function Pagination({startPage, setStartPage, currentPage, setCurrentPage, total
             ),
           )
         }
-        disabled={
-          startPage >= Math.floor((totalPage - 1) / pageGroupSize)
-        }
+        disabled={startPage >= Math.floor((totalPage - 1) / pageGroupSize)}
       >
-        {'>'}
-      </button>
-    </div>
+        <ChevronRight width="35px" />
+      </ChevronButton>
+    </Container>
   );
 }
 
 export default Pagination;
+
+const ChevronButton = styled.button`
+  cursor: pointer;
+  border: none;
+  background: none;
+`;
+
+const PageButton = styled.button<{ active?: boolean }>`
+  width: 30px;
+  height: 30px;
+  margin: 1%;
+  font-size: 0.9rem;
+  box-sizing: border-box;
+  border: none;
+  cursor: pointer;
+  text-align: center;
+  background-color: ${(props) => (props.active ? success : white)};
+  color: ${(props) => (props.active ? white : { black })};
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 10px;
+`;
+
+const PagesContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 200px;
+  padding-top: 4px;
+`;
