0c6e8653c3f83bea894ea2c63e62cea2f990c638
조다민 <jdm080620@gmail.com>
2024-02-06 10:47:43 +0900

    Fix(S10P12A601-295): 세션 연결해제 수정

--- frontend/src/components/visit/VisitRoom.tsx
+++ frontend/src/components/visit/VisitRoom.tsx
@@ -1,31 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { OpenVidu, Publisher, Session, Subscriber } from 'openvidu-browser';
+import React, { useEffect, useRef, useState } from 'react';
+import { OpenVidu, Publisher, Subscriber } from 'openvidu-browser';
 import UserVideo from './UserVideo';
 import useStreamStore from '../../stores/StreamStore';
 import useUserStore from '../../stores/UserStore';
 import { useNavigate, useParams } from 'react-router-dom';
 import postSession from '../../services/visit/postSession';
 import postToken from '../../services/visit/postToken';
+import html2canvas from 'html2canvas';
+import { styled } from 'styled-components';
 
 function VisitRoom() {
-  let OV: OpenVidu;
-  let session: Session;
-  let sessionId = '';
-  const myUserName = useUserStore().user?.name;
   const { subscriberList, addSubscriber, delSubscriber, resetSubscriberList } =
     useStreamStore();
+  const OV = new OpenVidu();
+  const session = OV.initSession();
+  const myUserName = useUserStore().user?.name;
   const navigator = useNavigate();
   const params = useParams<{ id: string }>();
-  sessionId = params.id ? params.id : '';
+  const sessionId = params.id ? params.id : '';
   console.log(sessionId, myUserName);
 
   const [publisher, setPublisher] = useState<Publisher | null>(null);
   const [isVideoEnabled, setIsVideoEnabled] = useState(true);
   const [isAudioEnabled, setIsAudioEnabled] = useState(true);
 
+  const captureRef = useRef<HTMLDivElement>(null);
+
   const joinSession = async () => {
     console.log('joinSession');
-    if (OV !== null) {
+    console.log(OV, session);
+    if (OV !== null && session !== null) {
+      console.log('session not null');
       const mySession = session;
 
       mySession.on('streamCreated', (event) => {
@@ -68,13 +73,9 @@ function VisitRoom() {
 
   function leaveSession() {
     console.log('leaving session');
-    const mySession = session;
-
-    if (mySession) {
-      mySession.disconnect();
-    }
 
     // Empty all properties...
+    session.disconnect();
     resetSubscriberList();
     setPublisher(null);
     navigator('/family/visit');
@@ -103,22 +104,37 @@ function VisitRoom() {
 
   function renderSubscriberList() {
     return subscriberList.map((sub) => (
-      <div key={sub.id}>
-        <span>{sub.id}</span>
+      <VideoOne key={sub.id}>
         <UserVideo streamManager={sub} />
-      </div>
+      </VideoOne>
     ));
+  }
+
+  async function capturePublisher() {
+    if (captureRef.current) {
+      try {
+        const canvas = await html2canvas(captureRef.current);
+        const imageDataURL = canvas.toDataURL('image/jpeg');
+        console.log(imageDataURL);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 
   useEffect(() => {
     const init = async () => {
-      OV = new OpenVidu();
-      session = OV.initSession();
       await postSession(sessionId);
       await joinSession();
       console.log('useEffect');
     };
     init();
+
+    return () => {
+      // unmount
+      console.log('컴포넌트가 화면에서 사라짐');
+      leaveSession();
+    };
   }, []);
 
   return (
@@ -127,11 +143,12 @@ function VisitRoom() {
       <button type="button" onClick={leaveSession}>
         나가기
       </button>
-      <div>
-        {publisher !== null ? <UserVideo streamManager={publisher} /> : null}
-        <div>위에는 나 밑에는 다른 사람들 {subscriberList.length}</div>
+      <VideoGroup>
+        <VideoOne ref={captureRef}>
+          {publisher !== null ? <UserVideo streamManager={publisher} /> : null}
+        </VideoOne>
         {renderSubscriberList()}
-      </div>
+      </VideoGroup>
       <div>
         <button type="button" onClick={toggleVideo}>
           카메라
@@ -139,9 +156,26 @@ function VisitRoom() {
         <button type="button" onClick={toggleAudio}>
           마이크
         </button>
+        <button type="button" onClick={capturePublisher}>
+          캡쳐
+        </button>
       </div>
     </div>
   );
+  8;
 }
 
 export default VisitRoom;
+
+const VideoGroup = styled.div`
+  height: 85vh;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+
+const VideoOne = styled.div`
+  display: inline-block;
+  height: 50%;
+`;
