6bec83fcaab4b52d5ab72cf8cb79efdb3830c6bb
Keesung <person813@gmail.com>
2024-02-07 10:22:15 +0900

    Feat(S10P12A601-305) : swagger CORS 해결

--- backend/src/main/java/com/ssafy/ViewCareFull/StaticRoutingConfiguration.java
+++ backend/src/main/java/com/ssafy/ViewCareFull/StaticRoutingConfiguration.java
@@ -19,7 +19,8 @@ public class StaticRoutingConfiguration implements WebMvcConfigurer {
   public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("https://i10a601.p.ssafy.io:8030", "https://i10a601.p.ssafy.io:8090",
-            "https://i10a601.p.ssafy.io:8091", "http://localhost:3000", "http://localhost:5173")
+            "https://i10a601.p.ssafy.io:8091", "http://localhost:3000", "http://localhost:5173",
+            "http://localhost:8080")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
         .allowCredentials(true)
         .exposedHeaders("Authorization");
