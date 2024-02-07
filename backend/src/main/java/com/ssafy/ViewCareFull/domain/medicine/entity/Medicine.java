91828e5a2f3969179c27d7f8b67b058045b29c52
Keesung <person813@gmail.com>
2024-02-07 11:02:22 +0900

    Feat(S10P12A601-310) : 메인 페이지 생성자 이슈

--- backend/src/main/java/com/ssafy/ViewCareFull/domain/medicine/entity/Medicine.java
+++ backend/src/main/java/com/ssafy/ViewCareFull/domain/medicine/entity/Medicine.java
@@ -6,12 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Medicine {
 
   @Id
