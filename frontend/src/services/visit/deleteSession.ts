0aa1d3d864cf39da5b370626b00387a00eef5856
jdm080620 <jdm080620@gmail.com>
2024-02-13 16:58:03 +0900

    Feat(S10P12A601-352): 면회 캡쳐 전송

--- frontend/src/services/visit/deleteSession.ts
+++ frontend/src/services/visit/deleteSession.ts
@@ -2,9 +2,7 @@ import api from '../api';
 
 async function deleteSession(sessionId: string) {
   try {
-    const response = await api.delete(
-      `/openvidu/sessions?sessionId=${sessionId}`,
-    );
+    const response = await api.delete(`/openvidu/sessions/${sessionId}`);
     return response.data;
   } catch (error) {
     console.log(error);
