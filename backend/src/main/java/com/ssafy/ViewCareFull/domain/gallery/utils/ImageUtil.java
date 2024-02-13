d0d9046baac6d0149413dd6cdffdb23072333aab
이기성 <dlrltjdwkd@naver.com>
2024-02-13 17:57:42 +0900

    Fix(S10P12A601-347) : jpg 수정 6

--- backend/src/main/java/com/ssafy/ViewCareFull/domain/gallery/utils/ImageUtil.java
+++ backend/src/main/java/com/ssafy/ViewCareFull/domain/gallery/utils/ImageUtil.java
@@ -49,7 +49,7 @@ public class ImageUtil {
         File outputFile = new File(outputImagePath.replace(".jpg", "_" + i + ".jpg"));
         imagePaths.add(outputFile.getAbsolutePath());
         // format jpg
-        ImageIO.write(fadedImage, "PNG", outputFile);
+        ImageIO.write(fadedImage, "jpg", outputFile);
         log.info("outputFile: " + outputFile.getAbsolutePath());
       }
     } catch (IOException e) {
