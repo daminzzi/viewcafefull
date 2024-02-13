81afe3fdcc469b330398f54ec564080a07fecda1
jiwoni1210 <jiwoni1210@naver.com>
2024-02-13 16:13:55 +0900

    Fix(S10P12A601-373): 카카오 로그인 라우터 수정

--- frontend/src/services/KakaoRedirect.tsx
+++ frontend/src/services/KakaoRedirect.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from './api';
 import useUserStore from '../stores/UserStore';
+import getConnectInfo from './connect/getConnectInfo';
+import useConnectStore from '../stores/ConnectStore';
 
 interface ResponseData {
   user: User | null;
@@ -12,6 +14,7 @@ interface ResponseData {
 function KakaoRedirect() {
   const navigate = useNavigate();
   const { setUser, setAccessToken } = useUserStore();
+  const { updateConnect } = useConnectStore();
 
   useEffect(() => {
     const url = new URL(window.location.href);
@@ -26,10 +29,23 @@ function KakaoRedirect() {
     // 로그인 시도 시 카카오토큰 유무 확인, 있으면 로그인 완료
     api
       .get<ResponseData>(`/users/kakao/signin?code=${kakaoToken}`)
-      .then((response) => {
-        setUser(response.data.user);
-        setAccessToken(response.data.accessToken);
-        navigate('/family');
+      .then(async (response) => {
+        const user = response.data.user;
+        const accessToken = response.data.accessToken;
+
+        setUser(user);
+        setAccessToken(accessToken);
+
+        if (user) {
+          const connectArray = await getConnectInfo('app', user.id);
+          updateConnect('app', user.id);
+
+          if (connectArray.length === 0) {
+            navigate('/connect/register');
+          } else if (connectArray.length !== 0) {
+            navigate('/family');
+          }
+        }
       })
       .catch((error) => {
         // 카카오로 가입되지 않은 사용자인 경우(카카오토큰이 없음)
