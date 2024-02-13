915a35d70b1a0a2f0b65d8e199303db25887a802
박정훈 <wjdgns1110@daum.net>
2024-02-13 13:56:16 +0900

    Fix(S10P12A601-328): FFmpeg command edit

--- backend/src/main/java/com/ssafy/ViewCareFull/domain/ffmpeg/service/FFmpegService.java
+++ backend/src/main/java/com/ssafy/ViewCareFull/domain/ffmpeg/service/FFmpegService.java
@@ -24,26 +24,33 @@ public class FFmpegService {
   // 비디오 파일이 저장될 경로
   @Value("${video.outputPath}")
   private String videoOutputPath;
+  // 오디오 파일이 저장된 경로
+  @Value("${audio.inputPath}")
+  private String audioInputPath;
 
   public void buildCommand(List<String> imageUrls) throws IOException, InterruptedException {
     // 이미지를 비디오로 변환하는 FFmpeg 명령어 생성
     StringBuilder ffmpegCommandBuilder = new StringBuilder();
+    int framrate = 30; // 초당 프레임 수 설정
+    int imageSec = 2; // 이미지가 보여지는 시간 설정
+    String audioStartSec = "30"; // 오디오 시작 시간 설정
     log.info("FFmpeg path: " + ffmpegPath);
     ffmpegCommandBuilder
         .append(ffmpegPath)
-        .append(" -framerate 30"); // 초당 프레임 수 설정
+        .append(" -framerate " + framrate); // 초당 프레임 수 설정
     for (String imageUrl : imageUrls) {
       ffmpegCommandBuilder.append(" -i ").append(imageUrl); // 각각의 이미지 사용 설정
     }
-    ffmpegCommandBuilder.append(" -filter_complex \""); // 여러개의 이미지를 하나로 결합하는 필터 사용
+    ffmpegCommandBuilder.append(" -i " + audioInputPath + "audio.mp3") // 오디오 사용 설정
+        .append(" -filter_complex \""); // 여러개의 이미지를 하나로 결합하는 필터 사용
     for (int i = 0; i < imageUrls.size(); i++) {
-      ffmpegCommandBuilder.append("[").append(i).append(":v]"); // index 번째 이미지 필터 설정
-      ffmpegCommandBuilder.append("loop=1"); // loop/framerate 초 만큼 각 이미지가 재생된다
-      ffmpegCommandBuilder.append(":size=1"); // 반복할 프레임 수 설정 - 1프레임만 반복
-      ffmpegCommandBuilder.append(":start=0"); // 반복 시작 시점 설정 - 0초부터 시작
-      ffmpegCommandBuilder.append(",scale=1920:1080"); // 이미지 크기 설정 - 1920x1080으로 설정
-      ffmpegCommandBuilder.append(",setsar=1"); // 화면 비율 설정 - 1:1로 설정
-      ffmpegCommandBuilder.append("[v").append(i).append("]; "); // index 번째 이미지 필터 설정
+      StringBuilder append = ffmpegCommandBuilder.append("[").append(i).append(":v]") // index 번째 이미지 필터 설정
+          .append("loop=" + imageSec * framrate) // loop/framerate 초 만큼 각 이미지가 재생된다
+          .append(":size=1") // 반복할 프레임 수 설정 - 1프레임만 반복
+          .append(":start=0") // 반복 시작 시점 설정 - 0초부터 시작
+          .append(",scale=1920:1080")// 이미지 크기 설정 - 1920x1080으로 설정 (반드시 짝수로)
+          .append(",setsar=1") // 화면 비율 설정 - 1:1로 설정
+          .append("[v").append(i).append("]; "); // index 번째 이미지 필터 설정
     }
     for (int i = 0; i < imageUrls.size(); i++) {
       ffmpegCommandBuilder.append("[v").append(i).append("]"); // 사용할 이미지 필터 명시
@@ -53,7 +60,16 @@ public class FFmpegService {
         .append(":v=1") // 비디오로 출력 설정
         .append(":a=0") // 오디오로 출력 미설정
         .append("[v]\"") // 이미지 결합 설정
+        .append("[" + imageUrls.size() + ":a]") // 오디오 사용 설정
+        .append("atrim=start=" + audioStartSec) // 오디오 시작 시간 설정
+        .append(":end=" + 2 * imageUrls.size()) // 오디오 종료 시간 설정
+        .append(",aloop=loop=1") // 오디오 반복 설정
+        .append(":size=1") // 오디오 반복 횟수 설정
+        .append(":start") // 오디오 처음부터 시작
+        .append("asetpts=PTS-STARTPTS[a_trimmed];") // 오디오 타임 스탬프 조정
+        .append("[a_trimmed]adelay=0|0[a]\"") // 오디오 딜레이 설정
         .append(" -map \"[v]\"") // 이미지 결합 사용 설정
+        .append(" -map \"[a]\"") // 오디오 결합 사용 설정
         .append(" -y ") // 저장 시 자동 덮어쓰기 설정
         .append(videoOutputPath + "video.mp4"); // 저장 경로 설정
 
